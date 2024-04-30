7dab1b478eab914d500b095b4a7bb3b478e0de74
nicollyclass <166552568+nicollyclass@users.noreply.github.com>
2024-04-30 14:59:54 -0300

    Create juros5.py

---
+++ juros5.py
@@ -0,0 +1,8 @@
+vp = float(input("digite o valor"))
+periodo = float(input("digite o periodo"))
+taxa_juros = float(input('digiteos juros'))
+
+valor_juros = vp*taxa_juros*periodo
+
+valor_final = vp + valor_juros
+print(valor_final)
