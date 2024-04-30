914f77a7515f07edd9c0d23504b44bc676fbee31
nicollyclass <166552568+nicollyclass@users.noreply.github.com>
2024-04-30 15:09:01 -0300

    Create perimetro4.py

---
+++ perimetro4.py
@@ -0,0 +1,8 @@
+base = float(input("digite a base do retangúlo"))
+altura = float(input("digite a altura do retangúlo"))
+
+area = float(base) * float(altura)
+perimetro = float(base)*2 + float(altura)*2
+
+print(area, "a area do retangúlo")
+print(perimetro,"o perimetro do retangúlo")
